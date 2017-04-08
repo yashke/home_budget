c92832d50c07b8d02fbf41a22137c0071fd4ce3b
Jan Filipowski <jachuf@gmail.com>
2017-04-08 07:43:14 +0200

    Handle file uploads without doing anything with them

--- backend/src/home_budget/main.go
+++ backend/src/home_budget/main.go
@@ -3,16 +3,28 @@ package main
 import (
   "github.com/gorilla/mux"
   "net/http"
+  "mime/multipart"
   "fmt"
 )
 
 func main() {
   router := mux.NewRouter()
-  router.HandleFunc("/", welcomeHandler)
+  router.HandleFunc("/account_balances", welcomeHandler)
   http.Handle("/", router)
   http.ListenAndServe("0.0.0.0:80", nil)
 }
 
-func welcomeHandler(writer http.ResponseWriter, response *http.Request) {
-  fmt.Println("Received request")
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+  request.ParseMultipartForm(32 << 20)
+  form := request.MultipartForm
+  uploadedFiles := form.File["file[]"]
+
+  for i, _ := range uploadedFiles {
+    parseUploadedFile(uploadedFiles[i])
+  }
+}
+
+func parseUploadedFile(uploadedFile *multipart.FileHeader) {
+  file, _ := uploadedFile.Open()
+  fmt.Println(file)
 }
