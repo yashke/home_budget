118b07c3f7e6a3264a43eda6874786f77ececc31
Jan Filipowski <jachuf@gmail.com>
2017-05-18 21:51:53 +0200

    Add LastDay to balance, to know what's the last entry date

--- backend/src/home_budget/account_balance.go
+++ backend/src/home_budget/account_balance.go
@@ -3,11 +3,13 @@ package main
 import (
   "github.com/jinzhu/gorm"
   _ "github.com/jinzhu/gorm/dialects/postgres"
+  "time"
 )
 
 type AccountBalance struct {
   gorm.Model
   AccountId string `gorm:"size:255"`
+  LastDay time.Time
   BalanceInCents int64
   BalanceCurrency string `gorm:"size:3"`
 }
